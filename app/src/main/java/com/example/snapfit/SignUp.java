84137c5074edded8e180f13fa419fb39e3558cf0
Imanthi Jayakody <imanthij777@gmail.com>
2022-04-21 10:48:56 +0530

    Adds initial signup page

--- app/src/main/java/com/example/snapfit/SignUp.java
+++ app/src/main/java/com/example/snapfit/SignUp.java
@@ -1,14 +1,14 @@
 package com.example.snapfit;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.sign_up);
     }
 }
