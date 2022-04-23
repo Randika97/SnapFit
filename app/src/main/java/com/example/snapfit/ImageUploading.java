85e4f1b974eebfedc50b63088894e2f814ba1151
Imanthi Jayakody <imanthij777@gmail.com>
2022-04-23 17:19:35 +0530

    Adds initial image upload page

---
+++ app/src/main/java/com/example/snapfit/ImageUploading.java
@@ -0,0 +1,20 @@
+package com.example.snapfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ImageUploading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_uploading);
+
+    }
+}
