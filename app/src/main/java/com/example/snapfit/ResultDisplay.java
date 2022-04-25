f528b75ea66f6d65bd7c21943ee161d92bc54e57
Imanthi Jayakody <imanthij777@gmail.com>
2022-04-25 17:08:45 +0530

    Adds initial results displaying page

---
+++ app/src/main/java/com/example/snapfit/ResultDisplay.java
@@ -0,0 +1,14 @@
+package com.example.snapfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_display);
+    }
+}
