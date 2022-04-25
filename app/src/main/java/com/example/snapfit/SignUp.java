f31166bae642feb37d5a5eeed3b9b76e3049d666
randika97 <randikasuridu@gmail.com>
2022-04-26 01:39:35 +0530

    Login and Signup connected with firebase

--- app/src/main/java/com/example/snapfit/SignUp.java
+++ app/src/main/java/com/example/snapfit/SignUp.java
@@ -1,14 +1,65 @@
 package com.example.snapfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
+    private EditText email, password,confimPassword;
+    private Button regBtn;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword2);
+        confimPassword = findViewById(R.id.editTextTextPassword3);
+        regBtn = findViewById(R.id.button);
+        regBtn.setOnClickListener(v -> registerNewUser());
+    }
+    private void registerNewUser() {
+
+        String firebaseEmail, firebasePassword;
+        firebaseEmail = email.getText().toString();
+        firebasePassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(firebaseEmail)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(firebasePassword)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(firebaseEmail, firebasePassword)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+
+                        Intent intent = new Intent(SignUp.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
