e8c20413fd5a2c414b23c4cd5ae19cdeb37dd56a
randika97 <randikasuridu@gmail.com>
2022-05-20 23:53:44 +0530

    Final commit

--- app/src/main/java/com/example/snapfit/ResultDisplay.java
+++ app/src/main/java/com/example/snapfit/ResultDisplay.java
@@ -7,9 +7,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.snapfit.model.Result;
@@ -24,7 +26,14 @@ import retrofit2.Response;
 public class ResultDisplay extends AppCompatActivity {
     DrawerLayout drawerLayout;
     String matchResult;
-    private TextView result;
+    String figure;
+    public TextView result;
+    public TextView clothText;
+    public ImageView recommondOne;
+    public ImageView recommondTwo;
+    public ImageView recommondThree;
+    public ImageView recommondFour;
+    public ImageView goBack;
     private static final String TAG = "ResultDisplay";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +41,56 @@ public class ResultDisplay extends AppCompatActivity {
         setContentView(R.layout.activity_result_display);
         drawerLayout = findViewById(R.id.drawer_layout);
         result = (TextView) findViewById(R.id.textView5);
+        clothText = (TextView) findViewById(R.id.clothImage2);
+        recommondOne = (ImageView) findViewById(R.id.clothImage1);
+        recommondTwo = (ImageView) findViewById(R.id.clothImage3);
+        recommondThree = (ImageView) findViewById(R.id.clothImage4);
+        recommondFour = (ImageView) findViewById(R.id.clothImage5);
+        goBack = (ImageView) findViewById(R.id.imageButton3);
+        goBack.setOnClickListener(v -> goBack());
         //checking the passed Country value from previous acitvity and set it to text
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if (extras != null) {
                 matchResult = extras.getString("result");
-                if(matchResult.equals("matching")){
+                figure = extras.getString("figure");
+                if(matchResult.equals("matching") && figure.equals("pearShape")){
                     result.setText("Matching");
+                    recommondOne.setImageResource(R.drawable.questiomark);
+                    recommondTwo.setImageResource(R.drawable.questiomark);
+                    recommondThree.setImageResource(R.drawable.questiomark);
+                    recommondFour.setImageResource(R.drawable.questiomark);
+                    clothText.setText(R.string.PearShapeSuggestion);
+                }else if(matchResult.equals("notmatching") && figure.equals("pearShape")){
+                    result.setText("Not-Matching");
+                    clothText.setText(R.string.PearShapeSuggestion);
+                    recommondOne.setImageResource(R.drawable.questiomark);
+                    recommondTwo.setImageResource(R.drawable.questiomark);
+                    recommondThree.setImageResource(R.drawable.questiomark);
+                    recommondFour.setImageResource(R.drawable.questiomark);
+                }else if(matchResult.equals("matching") && figure.equals("hourglassBody")){
+                    result.setText("Matching");
+                    clothText.setText(R.string.hourglassSuggestion);
+                    recommondOne.setImageResource(R.drawable.questiomark);
+                    recommondTwo.setImageResource(R.drawable.questiomark);
+                    recommondThree.setImageResource(R.drawable.questiomark);
+                    recommondFour.setImageResource(R.drawable.questiomark);
+                }else if(matchResult.equals("notmatching") && figure.equals("appleShape")){
+                    result.setText("Not-Matching");
+                    clothText.setText(R.string.appleShape);
+                    recommondOne.setImageResource(R.drawable.recommendtopone);
+                    recommondTwo.setImageResource(R.drawable.recommenedtoptwo);
+                    recommondThree.setImageResource(R.drawable.recommenedtopthree);
+                    recommondFour.setImageResource(R.drawable.recommenedtopfour);
+                }else if(matchResult.equals("matching") && figure.equals("appleShape")){
+                    result.setText("Matching");
+                    clothText.setText(R.string.applematch);
+                    recommondOne.setImageResource(R.drawable.questiomark);
+                    recommondTwo.setImageResource(R.drawable.questiomark);
+                    recommondThree.setImageResource(R.drawable.questiomark);
+                    recommondFour.setImageResource(R.drawable.questiomark);
                 }else{
+                    clothText.setText("No suggestion found");
                     result.setText("Not-Matching");
                 }
             } else {
@@ -47,9 +98,6 @@ public class ResultDisplay extends AppCompatActivity {
             }
         }
     }
-
-
-
     //Drawer functions
     public void ClickMenu(View view){
         //open drawer
@@ -86,6 +134,11 @@ public class ResultDisplay extends AppCompatActivity {
         //set flag
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.startActivity(intent);
+    }
+    //Move to SignUp UI
+    private void goBack() {
+        Intent intent = new Intent(ResultDisplay.this, ImageUploading.class);
+        startActivity(intent);
     }
 
 }
